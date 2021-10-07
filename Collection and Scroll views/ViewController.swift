b882e5f2211a7951dfa8feb0e295ad1f58b3a0a1
Khojimatov Anvarjon <khojimatov14@gmail.com>
2021-10-07 21:59:18 +0900

    Initial Commit

---
+++ Collection and Scroll views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection and Scroll views
+//
+//  Created by Anvarjon Khojimatov on 2021/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
